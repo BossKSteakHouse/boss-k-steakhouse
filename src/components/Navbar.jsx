07da87a9cbd2ac95994321c7c8c366b28a779f6e
PhilippeMallari <mallari.philippe@gmail.com>
2023-05-26 13:08:55 -0600

    navbar scaffolding

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container">
+                    <div className="col-8">
+                        <a class="navbar-brand" href="#">LOGO WILL GO HERE</a>
+                    </div>
+                    <div className="col-4">
+                        <a class="navbar-brand" href="#">FB Page</a>
+                        <a class="navbar-brand" href="#">Menu</a>
+                        <a class="navbar-brand" href="#">Home</a>
+                        <a class="navbar-brand" href="#">Contact Us</a>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+    }
