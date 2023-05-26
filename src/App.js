24957334aebe72e936fdeb7ba0854accf6ab8145
PhilippeMallari <mallari.philippe@gmail.com>
2023-05-26 16:41:58 -0600

    added navbar items style / added logo / added containers for main content

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ function App() {
   return (
     <>
       <Navbar />
+      <div class="container">
+        <div class="row">
+          <div className="col-6 mt-5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, praesentium? Voluptas pariatur explicabo culpa porro! Minus eius facere vero! Ipsa.
+          </div>
+          <div className="col-6 mt-5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, praesentium? Voluptas pariatur explicabo culpa porro! Minus eius facere vero! Ipsa.
+          </div>
+        </div>
+      </div>
+
+      <div class="container mt-5 ourMenu">
+        <h2 class="text-center text-white pt-2">OUR MENU</h2>
+        a picture here
+      </div>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,36 @@
 import React from 'react'
+import logo from '../assets/restaurant-logo.jpg'
 
 export default function Navbar() {
     return (
         <>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <nav class="navbar navbar-expand-lg navbar-light">
                 <div class="container">
-                    <div className="col-8">
-                        <a class="navbar-brand" href="#">LOGO WILL GO HERE</a>
-                    </div>
-                    <div className="col-4">
-                        <a class="navbar-brand" href="#">FB Page</a>
-                        <a class="navbar-brand" href="#">Menu</a>
-                        <a class="navbar-brand" href="#">Home</a>
-                        <a class="navbar-brand" href="#">Contact Us</a>
+                    <a className="navbar-brand" href="#">
+                    <img src={logo} alt="Logo" />
+                    </a>
+                    <button id="burger-menu" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto navItems">
+                        <li class="nav-item">
+                        <a class="nav-link text-white" href="#">FB Page</a>
+                        </li>
+                        <span className="space"></span>
+                        <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Menu</a>
+                        </li>
+                        <span className="space"></span>
+                        <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Home</a>
+                        </li>
+                        <span className="space"></span>
+                        <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Contact Us</a>
+                        </li>
+                        <span className="space"></span>
+                    </ul>
                     </div>
                 </div>
             </nav>
