dc00cc291d7bceb1bf6706a07189b763c420b2a0
PhilippeMallari <mallari.philippe@gmail.com>
2023-05-29 15:54:46 -0600

    added welcoming messages and location plus picture / menu added / testimonials added / contact us with google maps added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import Navbar from "./components/Navbar";
+import firstPicture from './assets/restaurant-hero-picture.jpg'
+import restaurantMenu from './assets/restaurant-menu.jpg'
+import testimonial1 from './assets/testimonials-1.PNG'
+import testimonial2 from './assets/testimonials-2.PNG'
+import testimonial3 from './assets/testimonials-3.PNG'
+
 
 function App() {
   return (
@@ -6,18 +12,126 @@ function App() {
       <Navbar />
       <div class="container">
         <div class="row">
-          <div className="col-6 mt-5">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, praesentium? Voluptas pariatur explicabo culpa porro! Minus eius facere vero! Ipsa.
+          <div class="col-6 mt-5 firstParagraph">
+            <span>
+              Steakhouse na UNLI RICE, UNLI SOUP at UNLI GRAVY nasa fairview na!
+              <br />
+              Tara boss! Bukas na kami!
+              <br />
+              Located at: Lot 2 Blk 11 Dahlia ave. Fairview, QC (Inside UNIOIL Gas Station)
+              3:00-11:00pm
+              Waze/Google Maps: BOSS K Steakhouse - Dahlia
+            </span>
+            <br />
           </div>
-          <div className="col-6 mt-5">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, praesentium? Voluptas pariatur explicabo culpa porro! Minus eius facere vero! Ipsa.
+          <div class="col-6 mt-5 px-0">
+            <img
+              src={firstPicture}
+              alt="firstPicture"
+              class="img-fluid firstPicture"
+              style={{ maxHeight: '300px' }}
+            />
           </div>
         </div>
       </div>
 
       <div class="container mt-5 ourMenu">
         <h2 class="text-center text-white pt-2">OUR MENU</h2>
-        a picture here
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col p-0">
+            <img
+              src={restaurantMenu}
+              alt="restaurantMenu"
+              class="img-fluid"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="container mt-5">
+        <div class="row testimonialsHeader">
+          <span>
+            TESTIMONIALS
+          </span>
+        </div>
+        <div class="row testimonialsSubtext">
+          <span>
+            Read What Others Have to Say
+          </span>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row gutters">
+          <div class="col-4">
+            <div class="card testimonialCard">
+              <img src={testimonial1} class="card-img-top" alt="Person 1" />
+              <div class="card-body">
+                <h3>Franz KatrionaEthan</h3>
+                <p class="card-text">“Yummy anjan ako kgabi Legit msarap”</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card testimonialCard">
+              <img src={testimonial2} class="card-img-top" alt="Person 2" />
+              <div class="card-body">
+                <h3>Meann Jinio</h3>
+                <p class="card-text">“The best po kc ang BOSS K Steakhouse - Dahlia, Fairview”</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card testimonialCard">
+              <img src={testimonial3} class="card-img-top" alt="Person 3" />
+              <div class="card-body">
+                <h3>daisy fust</h3>
+                <p class="card-text">“Sarap...”</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container mt-5">
+        <div class="row contactUsHeader">
+          <span>
+            CONTACT US
+          </span>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-6 mt-5 contactUsParagraph">
+            <span>
+              Located at: Lot 2 Blk 11 Dahlia ave. Fairview, QC (Inside UNIOIL Gas Station)
+            </span>
+            <br />
+            <span>
+              3:00-11:00pm
+            </span>
+            <span>
+              Waze/Google Maps: BOSS K Steakhouse - Dahlia
+            </span>
+            <br />
+            <button type="button" class="btn btn-secondary">
+              Boss K Steakhouse FB Page
+            </button>
+          </div>
+          <div class="col-6 mt-5 px-0">
+            <iframe
+              width="100%"
+              height="100%"
+              title="map"
+              className="absolute inset-0"
+              style={{ filter: "opacity(0.7)" }}
+              src="https://www.google.com/maps/embed/v1/place?q=Dahlia,+Fairview+QC+(Inside+UNIOIL+Dahlia)+1118+Quezon+City,+Philippines&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            />
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,31 +6,31 @@ export default function Navbar() {
         <>
             <nav class="navbar navbar-expand-lg navbar-light">
                 <div class="container">
-                    <a className="navbar-brand" href="#">
-                    <img src={logo} alt="Logo" />
+                    <a class="navbar-brand" href="#">
+                        <img src={logo} alt="Logo" class="navbarLogo" />
                     </a>
                     <button id="burger-menu" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                        <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto navItems">
-                        <li class="nav-item">
-                        <a class="nav-link text-white" href="#">FB Page</a>
-                        </li>
-                        <span className="space"></span>
-                        <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Menu</a>
-                        </li>
-                        <span className="space"></span>
-                        <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Home</a>
-                        </li>
-                        <span className="space"></span>
-                        <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Contact Us</a>
-                        </li>
-                        <span className="space"></span>
-                    </ul>
+                        <ul class="navbar-nav ms-auto navItems">
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="#">FB Page</a>
+                            </li>
+                            <span class="space"></span>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="#">Menu</a>
+                            </li>
+                            <span class="space"></span>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="#">Home</a>
+                            </li>
+                            <span class="space"></span>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="#">Contact Us</a>
+                            </li>
+                            <span class="space"></span>
+                        </ul>
                     </div>
                 </div>
             </nav>
